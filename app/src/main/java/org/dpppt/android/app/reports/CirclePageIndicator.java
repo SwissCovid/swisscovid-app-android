6b1cbe6435495d21a1beaf29372fa43dc4e5ef74
Simon Rösch <roesch@ubique.ch>
2020-04-30 22:14:54 +0200

    add copyright header to all files

--- app/src/main/java/org/dpppt/android/app/reports/CirclePageIndicator.java
+++ app/src/main/java/org/dpppt/android/app/reports/CirclePageIndicator.java
@@ -1,21 +1,10 @@
-package org.dpppt.android.app.reports;
-
 /*
- * Copyright (C) 2011 Patrik Akerfeldt
- * Copyright (C) 2011 Jake Wharton
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Created by Ubique Innovation AG
+ * https://www.ubique.ch
+ * Copyright (c) 2020. All rights reserved.
  */
+
+package org.dpppt.android.app.reports;
 
 import android.content.Context;
 import android.content.res.Resources;
