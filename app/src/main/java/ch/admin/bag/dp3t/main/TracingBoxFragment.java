a5b83034e148f767e038e82d7c4b33d68dfce984
Johannes Gallmann <gallmann@ubique.ch>
2020-07-22 18:00:16 +0200

    PP-637: Implemented Error Views for turned off tracing

--- app/src/main/java/ch/admin/bag/dp3t/main/TracingBoxFragment.java
+++ app/src/main/java/ch/admin/bag/dp3t/main/TracingBoxFragment.java
@@ -16,6 +16,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
@@ -90,11 +91,15 @@ public class TracingBoxFragment extends Fragment {
 			} else if (tracingStatusInterface.isReportedAsInfected()) {
 				tracingStatusView.setVisibility(View.VISIBLE);
 				tracingErrorView.setVisibility(View.GONE);
-				TracingStatusHelper.updateStatusView(tracingStatusView, TracingState.ENDED);
+				TracingStatusHelper.updateStatusView(tracingStatusView, TracingState.ENDED, isHomeFragment);
 			} else if (!isTracing) {
 				tracingStatusView.setVisibility(View.GONE);
 				tracingErrorView.setVisibility(View.VISIBLE);
-				TracingStatusHelper.showTracingDeactivated(tracingErrorView);
+				TracingStatusHelper.showTracingDeactivated(tracingErrorView, isHomeFragment);
+				TextView buttonView = tracingErrorView.findViewById(R.id.error_status_button);
+				buttonView.setOnClickListener(v -> {
+					enableTracing();
+				});
 			} else {
 				tracingStatusView.setVisibility(View.VISIBLE);
 				tracingErrorView.setVisibility(View.GONE);
