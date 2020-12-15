98e0147fabf22d3fc162cb92625a714dd515b72e
Johannes Gallmann <gallmann@ubique.ch>
2020-12-15 14:37:40 +0100

    PP-851: Implement Tracing Reactivation Reminder

---
+++ app/src/main/java/ch/admin/bag/dp3t/contacts/ReactivateTracingReminderDialog.java
@@ -0,0 +1,92 @@
+package ch.admin.bag.dp3t.contacts;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import ch.admin.bag.dp3t.R;
+import ch.admin.bag.dp3t.util.ReminderHelper;
+import ch.admin.bag.dp3t.viewmodel.TracingViewModel;
+
+public class ReactivateTracingReminderDialog extends DialogFragment implements CompoundButton.OnCheckedChangeListener {
+
+	public static ReactivateTracingReminderDialog newInstance() { return new ReactivateTracingReminderDialog(); }
+
+	private static final long HOUR_IN_MILLISECONDS = 1000L * 60 * 60;
+	private TracingViewModel tracingViewModel;
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		tracingViewModel = new ViewModelProvider(requireActivity()).get(TracingViewModel.class);
+	}
+
+	@Override
+	public void onResume() {
+		getDialog().getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+		super.onResume();
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+			@Nullable Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.dialog_reactivate_tracing_reminder, container);
+	}
+
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		RadioGroup reminderRadioGroup = view.findViewById(R.id.reactivate_tracing_reminder_radio_group);
+		RadioButton fourHoursButton = view.findViewById(R.id.reactivate_tracing_reminder_radio_option_four_hours);
+		RadioButton eightHoursButton = view.findViewById(R.id.reactivate_tracing_reminder_radio_option_eight_hours);
+		RadioButton twelveHoursButton = view.findViewById(R.id.reactivate_tracing_reminder_radio_option_twelve_hours);
+		RadioButton noReminderButton = view.findViewById(R.id.reactivate_tracing_reminder_radio_option_no_reminder);
+
+		fourHoursButton.setOnCheckedChangeListener(this);
+		eightHoursButton.setOnCheckedChangeListener(this);
+		twelveHoursButton.setOnCheckedChangeListener(this);
+		noReminderButton.setOnCheckedChangeListener(this);
+		reminderRadioGroup.check(R.id.reactivate_tracing_reminder_radio_option_four_hours);
+
+		view.findViewById(R.id.reactivate_tracing_reminder_cancel_button).setOnClickListener(v -> {
+			tracingViewModel.invalidateTracingStatus();
+			dismiss();
+		});
+		view.findViewById(R.id.reactivate_tracing_reminder_ok_button).setOnClickListener(v -> {
+			tracingViewModel.disableTracing();
+			switch (reminderRadioGroup.getCheckedRadioButtonId()) {
+				case R.id.reactivate_tracing_reminder_radio_option_four_hours:
+					ReminderHelper
+							.setTracingActivationReminder(System.currentTimeMillis() + 4 * HOUR_IN_MILLISECONDS, requireContext());
+					break;
+				case R.id.reactivate_tracing_reminder_radio_option_eight_hours:
+					ReminderHelper
+							.setTracingActivationReminder(System.currentTimeMillis() + 8 * HOUR_IN_MILLISECONDS, requireContext());
+					break;
+				case R.id.reactivate_tracing_reminder_radio_option_twelve_hours:
+					ReminderHelper
+							.setTracingActivationReminder(System.currentTimeMillis() + 12 * HOUR_IN_MILLISECONDS,
+									requireContext());
+					break;
+				default:
+					break;
+			}
+			dismiss();
+		});
+	}
+
+	@Override
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		buttonView.setTypeface(isChecked ? Typeface.DEFAULT_BOLD : Typeface.DEFAULT);
+	}
+
+}
--- app/src/main/java/ch/admin/bag/dp3t/util/NotificationUtil.java
+++ app/src/main/java/ch/admin/bag/dp3t/util/NotificationUtil.java
@@ -7,23 +7,32 @@
  *
  * SPDX-License-Identifier: MPL-2.0
  */
-
 package ch.admin.bag.dp3t.util;
 
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.TaskStackBuilder;
 
+import ch.admin.bag.dp3t.MainActivity;
 import ch.admin.bag.dp3t.R;
 
 public class NotificationUtil {
 
+	public static final String ACTION_ACTIVATE_TRACING = "ACTION_ACTIVATE_TRACING";
+
 	public static final String NOTIFICATION_CHANNEL_ID = "contact-channel";
+	public static final String CHANNEL_ID_REMINDER = "CHANNEL_ID_REMINDER";
 	public static final int NOTIFICATION_ID_CONTACT = 42;
 	public static final int NOTIFICATION_ID_UPDATE = 43;
+	public static final int NOTIFICATION_ID_REMINDER = 44;
+
 
 	@RequiresApi(api = Build.VERSION_CODES.O)
 	public static void createNotificationChannel(Context context) {
@@ -33,6 +42,45 @@ public class NotificationUtil {
 				new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_HIGH);
 		channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
 		notificationManager.createNotificationChannel(channel);
+	}
+
+	public static void showReminderNotification(Context context) {
+		createNotificationChannel(CHANNEL_ID_REMINDER, context.getString(R.string.android_reminder_channel_name), context);
+		PendingIntent pendingIntent = createReminderPendingIntent(context);
+		String title = context.getString(R.string.tracing_reminder_notification_title);
+		String message = context.getString(R.string.tracing_reminder_notification_subtitle);
+
+		Notification notification = createNotification(title, message, pendingIntent, CHANNEL_ID_REMINDER, context);
+		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		notificationManager.notify(message.hashCode(), notification);
+	}
+
+	private static void createNotificationChannel(String channelId, String channelName, Context context) {
+		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		if (Build.VERSION.SDK_INT >= 26) {
+			NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+			notificationManager.createNotificationChannel(channel);
+		}
+	}
+
+	private static PendingIntent createReminderPendingIntent(Context context) {
+		Intent intent = new Intent(context, MainActivity.class);
+		intent.setAction(ACTION_ACTIVATE_TRACING);
+		return TaskStackBuilder.create(context)
+				.addNextIntentWithParentStack(intent)
+				.getPendingIntent(NOTIFICATION_ID_REMINDER, PendingIntent.FLAG_UPDATE_CURRENT);
+	}
+
+	private static Notification createNotification(String title, String message, PendingIntent pendingIntent, String channelId,
+			Context context) {
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId)
+				.setContentIntent(pendingIntent)
+				.setAutoCancel(true)
+				.setContentTitle(title)
+				.setContentText(message)
+				.setSmallIcon(R.drawable.ic_begegnungen)
+				.setStyle(new NotificationCompat.BigTextStyle().bigText(message));
+		return builder.build();
 	}
 
 }
