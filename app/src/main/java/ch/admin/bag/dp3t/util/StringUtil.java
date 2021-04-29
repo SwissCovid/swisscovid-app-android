be6c70103c2d215f629ad92fdfe1de97df3067af
Johannes Gallmann <gallmann@ubique.ch>
2021-04-29 09:34:16 +0200

    Implement Checked-In State on Homescreen

--- app/src/main/java/ch/admin/bag/dp3t/util/StringUtil.java
+++ app/src/main/java/ch/admin/bag/dp3t/util/StringUtil.java
@@ -7,7 +7,6 @@
  *
  * SPDX-License-Identifier: MPL-2.0
  */
-
 package ch.admin.bag.dp3t.util;
 
 import android.graphics.Typeface;
@@ -19,13 +18,16 @@ import androidx.annotation.NonNull;
 
 import java.math.BigInteger;
 import java.util.Calendar;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class StringUtil {
+
+	private static final long ONE_HOUR = TimeUnit.HOURS.toMillis(1);
 
 	/**
 	 * Creates a spannable where the {@code boldString} is set to bold within the {@code fullString}.
 	 * Be aware that this only applies to the first occurence.
-	 *
 	 * @param fullString The entire string
 	 * @param boldString The partial string to be made bold
 	 * @return A partially bold spannable
@@ -48,7 +50,7 @@ public class StringUtil {
 		BigInteger bi = new BigInteger(1, array);
 		String hex = bi.toString(16);
 		int paddingLength = (array.length * 2) - hex.length();
-		if(paddingLength > 0)
+		if (paddingLength > 0)
 			return String.format("%0" + paddingLength + "d", 0) + hex;
 		else
 			return hex;
@@ -65,6 +67,38 @@ public class StringUtil {
 			return "0" + timeUnit;
 		} else {
 			return String.valueOf(timeUnit);
+		}
+	}
+
+	/**
+	 * Formats a duration in milliseconds to a String of hours, minutes and seconds, or to only hours and minutes if the
+	 * duration is more than 10 hours
+	 * @param duration in milliseconds
+	 * @return a formatted duration String
+	 */
+	public static String getShortDurationString(long duration) {
+		if (duration >= TimeUnit.HOURS.toMillis(10)) {
+			return String.format(Locale.GERMAN, "%d:%02d",
+					TimeUnit.MILLISECONDS.toHours(duration),
+					TimeUnit.MILLISECONDS.toMinutes(duration - TimeUnit.HOURS.toMillis(TimeUnit.MILLISECONDS.toHours(duration)))
+			);
+		} else {
+			return getDurationString(duration);
+		}
+	}
+
+	public static String getDurationString(long duration) {
+		if (duration >= ONE_HOUR) {
+			return String.format(Locale.GERMAN, "%d:%02d:%02d",
+					TimeUnit.MILLISECONDS.toHours(duration),
+					TimeUnit.MILLISECONDS.toMinutes(duration - TimeUnit.HOURS.toMillis(TimeUnit.MILLISECONDS.toHours(duration))),
+					TimeUnit.MILLISECONDS.toSeconds(duration - TimeUnit.MINUTES.toMillis(TimeUnit.MILLISECONDS.toMinutes(duration)))
+			);
+		} else {
+			return String.format(Locale.GERMAN, "%02d:%02d",
+					TimeUnit.MILLISECONDS.toMinutes(duration),
+					TimeUnit.MILLISECONDS.toSeconds(duration - TimeUnit.MINUTES.toMillis(TimeUnit.MILLISECONDS.toMinutes(duration)))
+			);
 		}
 	}
 
