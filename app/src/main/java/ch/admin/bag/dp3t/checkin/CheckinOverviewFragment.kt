ceac631091f2c7d2d225a8555734cb73c27f2df5
Florian Buenzli <buenzli@ubique.ch>
2021-05-04 15:10:57 +0200

    added card and biometrics for diary screen

--- app/src/main/java/ch/admin/bag/dp3t/checkin/CheckinOverviewFragment.kt
+++ app/src/main/java/ch/admin/bag/dp3t/checkin/CheckinOverviewFragment.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import ch.admin.bag.dp3t.R
 import ch.admin.bag.dp3t.checkin.checkinflow.CheckOutFragment
 import ch.admin.bag.dp3t.checkin.checkinflow.QrCodeScannerFragment
+import ch.admin.bag.dp3t.checkin.diary.DiaryFragment
 import ch.admin.bag.dp3t.databinding.FragmentCheckinOverviewBinding
 import ch.admin.bag.dp3t.util.StringUtil
 
@@ -28,7 +32,7 @@ class CheckinOverviewFragment : Fragment() {
 		return FragmentCheckinOverviewBinding.inflate(layoutInflater).apply {
 			checkinOverviewToolbar.setNavigationOnClickListener { requireActivity().supportFragmentManager.popBackStack() }
 
-			checkinOverviewHistory.setOnClickListener { v: View? -> }
+			checkinOverviewDiary.setOnClickListener { authenticateAndShowDiary() }
 
 			crowdNotifierViewModel.isCheckedIn.observe(viewLifecycleOwner, { isCheckedIn ->
 				checkoutView.isVisible = isCheckedIn
@@ -60,6 +64,36 @@ class CheckinOverviewFragment : Fragment() {
 			.setCustomAnimations(R.anim.slide_enter, R.anim.slide_exit, R.anim.slide_pop_enter, R.anim.slide_pop_exit)
 			.replace(R.id.main_fragment_container, QrCodeScannerFragment.newInstance())
 			.addToBackStack(QrCodeScannerFragment::class.java.canonicalName)
+			.commit()
+	}
+
+	private fun authenticateAndShowDiary() {
+		val executor = ContextCompat.getMainExecutor(requireContext())
+		val biometricPrompt = BiometricPrompt(requireActivity(), executor, object : BiometricPrompt.AuthenticationCallback() {
+			override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+				showDiaryFragment()
+			}
+		})
+
+		if (BiometricManager.from(requireContext()).canAuthenticate(BiometricManager.Authenticators.DEVICE_CREDENTIAL or BiometricManager.Authenticators.BIOMETRIC_WEAK) == BiometricManager.BIOMETRIC_SUCCESS
+		) {
+			val promptInfo = BiometricPrompt.PromptInfo.Builder()
+				.setTitle(getString(R.string.authenticate_for_diary))
+				.setAllowedAuthenticators(
+					BiometricManager.Authenticators.DEVICE_CREDENTIAL or BiometricManager.Authenticators.BIOMETRIC_WEAK
+				)
+				.build()
+			biometricPrompt.authenticate(promptInfo)
+		} else {
+			showDiaryFragment()
+		}
+	}
+
+	private fun showDiaryFragment() {
+		requireActivity().supportFragmentManager.beginTransaction()
+			.setCustomAnimations(R.anim.slide_enter, R.anim.slide_exit, R.anim.slide_pop_enter, R.anim.slide_pop_exit)
+			.replace(R.id.main_fragment_container, DiaryFragment.newInstance())
+			.addToBackStack(DiaryFragment::class.java.canonicalName)
 			.commit()
 	}
 }
---
+++ app/src/main/java/ch/admin/bag/dp3t/checkin/diary/DiaryFragment.kt
@@ -0,0 +1,21 @@
+package ch.admin.bag.dp3t.checkin.diary
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DiaryFragment : Fragment() {
+
+	companion object {
+		fun newInstance(): DiaryFragment {
+			return DiaryFragment()
+		}
+	}
+
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+		TODO()
+	}
+
+}
