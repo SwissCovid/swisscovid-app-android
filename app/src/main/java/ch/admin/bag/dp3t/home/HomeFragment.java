fb05e6d15c2eaf818ef0dcaf3334792b2f151267
Johannes Gallmann <gallmann@ubique.ch>
2021-06-01 19:26:20 +0200

    Implemented Error Popup when entering Covidcode while checked in

--- app/src/main/java/ch/admin/bag/dp3t/home/HomeFragment.java
+++ app/src/main/java/ch/admin/bag/dp3t/home/HomeFragment.java
@@ -472,11 +472,24 @@ public class HomeFragment extends Fragment {
 				covidCodeTitle.setText(R.string.home_covidcode_card_title);
 				covidCodeText.setText(R.string.home_covidcode_card_text);
 				covidCodeButton.setOnClickListener(v -> {
-					Intent intent = new Intent(getActivity(), InformActivity.class);
-					startActivity(intent);
+					if (crowdNotifierViewModel.isCheckedIn().getValue()) {
+						showCannotEnterCovidcodeWhileCheckedInDialog();
+					} else {
+						Intent intent = new Intent(getActivity(), InformActivity.class);
+						startActivity(intent);
+					}
 				});
 			}
 		});
+	}
+
+	private void showCannotEnterCovidcodeWhileCheckedInDialog() {
+		new AlertDialog.Builder(requireContext(), R.style.NextStep_AlertDialogStyle)
+				.setMessage(R.string.error_cannot_enter_covidcode_while_checked_in)
+				.setPositiveButton(R.string.checkout_button_title, (dialog, id) -> showCheckOutFragment())
+				.setNegativeButton(R.string.cancel, (dialog, id) -> dialog.dismiss())
+				.create()
+				.show();
 	}
 
 	private void showCheckOutFragment() {
