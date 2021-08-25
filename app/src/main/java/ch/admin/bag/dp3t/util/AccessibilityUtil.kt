3796f457281fba20eedbd65965088149b0f9c6df
Florian Buenzli <buenzli@ubique.ch>
2021-08-25 22:54:25 +0200

    focus heading instead of covidcode input form (15)

--- app/src/main/java/ch/admin/bag/dp3t/util/AccessibilityUtil.kt
+++ app/src/main/java/ch/admin/bag/dp3t/util/AccessibilityUtil.kt
@@ -2,10 +2,25 @@
 
 package ch.admin.bag.dp3t.util
 
+import android.content.Context
 import android.view.View
 import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityManager
 import android.view.accessibility.AccessibilityNodeInfo
 import android.widget.Button
+import androidx.core.view.doOnLayout
+
+fun Context.isAccessibilityActive(): Boolean {
+	val am = getSystemService(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager?
+	return am != null && am.isEnabled && am.isTouchExplorationEnabled
+}
+
+fun View.requestAccessibilityFocus() {
+	doOnLayout {
+		performAccessibilityAction(AccessibilityNodeInfo.ACTION_ACCESSIBILITY_FOCUS, null)
+		sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_SELECTED)
+	}
+}
 
 fun View.setExpansionToggleStateAccessibilityDelegate(expandableView: View) {
 	accessibilityDelegate = object : View.AccessibilityDelegate() {
